08ef86e418a1753ec6e6b208c4e7e8149fb5825e
Stephanie J. Spielman <spielman@rowan.edu>
2019-03-18 19:05:47 -0400

    inst directory must also be named dragon so shiny deployment works

--- R/rundragon.R
+++ R/rundragon.R
@@ -2,9 +2,9 @@
 #'
 #' @export
 rundragon <- function() {
-  appDir <- system.file("dragonapp", package = "dragon")
+  appDir <- system.file("dragon", package = "dragon")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `dragon` from github with `remotes::install_github('spielmanlab/dragon').", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
