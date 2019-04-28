5ccb49316a794256dc2b5a5b0578d226692177d0
Stephanie J. Spielman <spielman@rowan.edu>
2019-04-28 13:10:05 -0400

    by god i think ive done it. please?

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
-Sys.setenv("R_TESTS" = "") ## Workaround for file issues from https://github.com/r-lib/testthat/issues/86
+#Sys.setenv("R_TESTS" = "") ## Workaround for file issues from https://github.com/r-lib/testthat/issues/86
 
 library(testthat)
 library(tidyverse)
 library(dragon)
 
-build_net <- system.file("inst/dragon/", "build_network.R", package = "dragon")
+build_net <- system.file("dragon/build_network.R", package = "dragon")
 print(build_net)
 source(build_net)
 test_check("dragon")
