e08a4e7c43dfded8a34e081b2751fe4ef522f190
Stephanie J. Spielman <spielman@rowan.edu>
2020-05-24 17:45:47 -0400

    towards a maybe favicon

---
+++ inst/app/www/make_sticker.R
@@ -0,0 +1,8 @@
+library(hexSticker)
+sticker("network_for_sticker_SO.png", 
+        package="", 
+        p_size=8, p_x = 1, p_y = 1.5, p_color="grey80",
+        s_x=1.2, s_y=0.8, s_width=2.5, s_height=4, 
+        filename = "favicon.png", 
+        h_fill = "grey20", h_color = "firebrick", 
+        dpi=1000)
