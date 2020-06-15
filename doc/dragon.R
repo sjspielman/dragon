60e300c0f0657703c22dd658a40b48ddda0d88f3
Stephanie J. Spielman <spielman@rowan.edu>
2020-06-15 17:46:08 -0400

    ignore doc and meta from rbuild, but i'd like to keep doc in git

---
+++ doc/dragon.R
@@ -0,0 +1,20 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  ## Install from CRAN:
+#  install.packages("dragon")
+#  
+#  ## Install from github:
+#  library(remotes)
+#  remotes::install_github("spielmanlab/dragon")
+
+## -----------------------------------------------------------------------------
+#  ## Launch application
+#  library(dragon)
+#  run_dragon() ## run_app() also works!
+
