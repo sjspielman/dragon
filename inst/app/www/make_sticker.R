d89a211ad069cb6b183728559d313af848a592a3
Stephanie J. Spielman <spielman@rowan.edu>
2020-07-04 10:12:31 -0400

    hex background is dragon theme

--- inst/app/www/make_sticker.R
+++ inst/app/www/make_sticker.R
@@ -4,6 +4,6 @@ sticker("download.png", #"FeO-4.6-force_for_sticker.png", #"dragon_for_sticker.p
         p_size=12, p_x = 1, p_y = 1.4, p_color="grey20",
         s_x=1.2, s_y=0.63, s_width=1.6, s_height=1, 
         filename = "favicon.png", 
-        h_fill = "#9f1e1f", h_color = "grey20",
+        h_fill = "#DE4C39", h_color = "grey20",
         #h_fill = "grey70", h_color = "firebrick", 
         dpi=2000)
