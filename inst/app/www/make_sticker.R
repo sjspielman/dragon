cae8bb5eb9379a34156cfb4a420674301dc38648
Stephanie J. Spielman <spielman@rowan.edu>
2020-07-04 10:10:38 -0400

    updated facivon to be red

--- inst/app/www/make_sticker.R
+++ inst/app/www/make_sticker.R
@@ -1,8 +1,9 @@
 library(hexSticker)
-sticker("FeO-4.6-force_for_sticker.png", #"dragon_for_sticker.png", #"network_for_sticker_SO.png", 
+sticker("download.png", #"FeO-4.6-force_for_sticker.png", #"dragon_for_sticker.png", #"network_for_sticker_SO.png", 
         package="dragon", 
-        p_size=10.5, p_x = 1, p_y = 1.5, p_color="grey20",
-        s_x=1, s_y=0.7, s_width=1.6, s_height=1, 
+        p_size=12, p_x = 1, p_y = 1.4, p_color="grey20",
+        s_x=1.2, s_y=0.63, s_width=1.6, s_height=1, 
         filename = "favicon.png", 
-        h_fill = "grey70", h_color = "firebrick", 
-        dpi=400)
+        h_fill = "#9f1e1f", h_color = "grey20",
+        #h_fill = "grey70", h_color = "firebrick", 
+        dpi=2000)
