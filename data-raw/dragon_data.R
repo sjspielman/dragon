9882ce6a6f66763d2128bdb0b18025e5009d3bff
Stephanie J. Spielman <spielman@rowan.edu>
2020-10-19 10:49:24 -0400

    Palette bug fix

--- data-raw/dragon_data.R
+++ data-raw/dragon_data.R
@@ -1,6 +1,7 @@
 ## code to prepare internal datasets goes here
 
-library(dragon)
+#library(dragon)
+devtools::load_all()
 
 ## LAST UPDATED ON 6/27/20 WITH MED 2/3/20 ##
 
@@ -8,8 +9,29 @@ med_data_cache <- fetch_med_data()
 element_redox_states_cache <- calculate_element_redox_states(med_data_cache)
 med_cache_date <- find_most_recent_date()
      
+     
+     
+
+# Tibble containing all RColorBrewer palettes used in dragon node coloring
+RColorBrewer::brewer.pal.info %>% 
+  tibble::rownames_to_column("name") %>%
+  dplyr::filter(colorblind == TRUE) %>%
+  dplyr::mutate(img = paste0("<img src='www/palette_png/", 
+                             name, 
+                             ".png' width=110px><div class='palette-style'>",
+                             name, 
+                             "</div></img>")) %>%
+  dplyr::arrange(dplyr::desc(category), name)-> palettes_of_interest
+
+palettes_of_interest %>% dplyr::filter(category == "qual") -> qual_palettes_ui
+
+palettes_of_interest %>% dplyr::filter(category != "qual") -> sd_palettes_ui
+
+
 usethis::use_data(med_data_cache,
                   element_redox_states_cache,
                   med_cache_date,
+                  qual_palettes_ui,
+                  sd_palettes_ui,
                   internal = TRUE, overwrite = TRUE, compress = "bzip2")
 
