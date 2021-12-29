2bf169b75d93a28367e6cf165e0573ee5b81b17f
Stephanie J. Spielman <spielman@rowan.edu>
2021-12-29 08:39:01 -0500

    don't ignore doc, we need the doc

---
+++ doc/dragon.R
@@ -0,0 +1,25 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  ## Install from CRAN:
+#  install.packages("dragon")
+#  
+#  ## Install from github (current release)
+#  #install.packages("remotes")
+#  remotes::install_github("sjspielman/dragon")
+#  
+#  
+#  ## Install from github for _experimental and unguaranteed_ features:
+#  #install.packages("remotes")
+#  remotes::install_github("sjspielman/dragon", ref = "dev")
+
+## -----------------------------------------------------------------------------
+#  ## Launch application
+#  library(dragon)
+#  run_dragon() ## run_app() also works!
+
